5e54f4faaaccb1c3123c4e240a2eec8d69fb033e
Shanto Paul <shantopaul470@gmail.com>
2024-09-24 12:25:29 +0600

    fixing responsive issue

--- js/back-to-old.js
+++ js/back-to-old.js
@@ -1,6 +1,6 @@
 document.getElementById('btn-home').addEventListener('click',function(){
     window.location.href ='/Assginment_05/index.html'   
  })
- document.getElementById('btn-home').addEventListener('click',function(){
+ document.getElementById('btn-home-sm').addEventListener('click',function(){
     window.location.href ='/Assginment_05/index.html'   
  })
